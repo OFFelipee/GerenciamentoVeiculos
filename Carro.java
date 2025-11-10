ea4c69d54025e5a0ea75d3dd944c88ea070d9856
Felipe da Silva Pinto <dasilvapintofelipe840@gmail.com>
2025-11-09 21:29:40 -0300

    Commit inicial: adicionando arquivos do projeto de gerenciamento de veículos

---
+++ Carro.java
@@ -0,0 +1,19 @@
+public class Carro extends Veiculo {
+    public int numeroPortas;
+
+    public Carro() {}
+
+    public Carro(String marca, String modelo, int ano, int numeroPortas) {
+        super(marca, modelo, ano);
+        this.numeroPortas = numeroPortas;
+    }
+
+    @Override
+    public String informacoesVeiculo() {
+        return "Carro: marca=" + marca +
+               ", modelo=" + modelo +
+               ", ano=" + getAno() +
+               ", numeroPortas=" + numeroPortas;
+    }
+}
+
---
+++ Moto.java
@@ -0,0 +1,26 @@
+public class Moto extends Veiculo {
+    private int cilindrada;
+
+    public Moto() {}
+
+    public Moto(String marca, String modelo, int ano, int cilindrada) {
+        super(marca, modelo, ano);
+        this.cilindrada = cilindrada;
+    }
+
+    public int getCilindrada() {
+        return cilindrada;
+    }
+
+    public void setCilindrada(int cilindrada) {
+        this.cilindrada = cilindrada;
+    }
+
+    @Override
+    public String informacoesVeiculo() {
+        return "Moto: marca=" + marca +
+               ", modelo=" + modelo +
+               ", ano=" + getAno() +
+               ", cilindrada=" + cilindrada + "cc";
+    }
+}
